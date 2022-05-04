aebf1d9a8f9dd8889d55b38c201025946a103980
scanlon129 <mscanlon@butlertill.com>
2022-05-04 17:07:31 -0400

    Add experimental web classes

--- Self Training & Scrap Work/web_interaction.py
+++ Self Training & Scrap Work/web_interaction.py
@@ -2,10 +2,33 @@
 PURPOSE:
 
 '''
-class Login
+class Exporter:
+    pass
 
-def __init__(self, title):
-    self.title = title
+class Websites_List:
+    def __init__(self, sites_type):
+        self.sites_type = sites_type
+        self.websites = []
 
-def login(uname, pword):
-    uname = 
+    def add(self, website):
+       self.websites.append(website)
+    
+    def delete(self, website):
+        self.websites.pop(website)
+
+class Website:
+    name: str = ''
+    uname: str = ''
+    pword: str = ''
+    url: str = ''
+    def __init__(self, name, uname, pword, url):
+        self.name = name
+        self.uname = uname
+        self.pword = pword
+        self.url = url
+
+facebook = Website('facebook', 'test@facebook.com', 'fbookpword', 'www.facebook.com')
+Websites_List.add(facebook)
+
+print(Websites_List)
+print(facebook)
