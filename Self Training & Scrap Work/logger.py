b09b639990a0abd5b6fe889fa1aa919349ab14ed
Matt Scanlon <mscanlon@butlertill.com>
2022-06-06 13:25:39 -0400

    Correct log_path reference

--- Self Training & Scrap Work/logger.py
+++ Self Training & Scrap Work/logger.py
@@ -13,12 +13,12 @@ log_path = 'C:\\Users\\scanl\\OneDrive - BUTLER-TILL MEDIA SERVICES\\\
 # Set overall logging format
 formatter = logging.formatter('%(asctime)s: %(message)s')
 # Set file logging filename, level and format
-log_file_handler = logging.FileHandler(logpath + 'debug.log')
+log_file_handler = logging.FileHandler(log_path + 'debug.log')
 log_file_handler.setLevel(logging.DEBUG)
 log_file_handler.setFormatter(formatter)
 logger.addHandler(log_file_handler)
 #Set error file logging filename, level and format
-error_log_file_handler = logging.FileHandler(logpath + 'errors.log')
+error_log_file_handler = logging.FileHandler(log_path + 'errors.log')
 error_log_file_handler.setLevel(logging.ERROR)
 error_log_file_handler.setFormatter(formatter)
 logger.addHandler(error_log_file_handler)
